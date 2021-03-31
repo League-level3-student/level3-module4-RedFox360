8000f8b15e35e1bad6898d34b68fdae8692b434e
RedFox360 <67213870+RedFox360@users.noreply.github.com>
2021-03-30 17:01:02 -0700

    03/30/21

--- src/_00_IntroToStacks/_01_IntroToStack.java
+++ src/_00_IntroToStacks/_01_IntroToStack.java
@@ -1,22 +1,33 @@
 package _00_IntroToStacks;
 
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Stack;
-
 import javax.swing.JOptionPane;
 
 public class _01_IntroToStack {
     public static void main(String[] args) {
         // 1. Create a Stack of Doubles
         //    Don't forget to import the Stack class
-
+    	Stack<Double> doubles = new Stack<Double>();
         // 2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-
+    	
+    	for (int i = 0; i < 100; i++) {
+    		doubles.push((double) new Random().nextInt(101));
+    	}
         // 3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-
+    	System.out.println("Enter two numbers between 0 and 100");
+    	System.out.println("Number 1:");
+    	Scanner scan = new Scanner(System.in);
+    	doubles.push(scan.nextDouble());
+    	System.out.println("Number 2:");
+    	doubles.push(scan.nextDouble());
+    	scan.close();
         // 4. Pop all the elements off of the Stack. Every time a double is popped that is
         //    between the two numbers entered by the user, print it to the screen.
-
+    	for (int i = 0; i < doubles.size(); i++) {
+    		System.out.println(doubles.pop());
+    	}
 
         // EXAMPLE:
         // NUM 1: 65
--- src/_00_IntroToStacks/_02_TextUndoRedo.java
+++ src/_00_IntroToStacks/_02_TextUndoRedo.java
@@ -1,21 +1,69 @@
 package _00_IntroToStacks;
 
-public class _02_TextUndoRedo {
-    /* 
-     * Create a JFrame with a JPanel and a JLabel.
-     * 
-     * Every time a key is pressed, add that character to the JLabel. It should
-     * look like a basic text editor.
-     * 
-     * Make it so that every time the BACKSPACE key is pressed, the last
-     * character is erased from the JLabel.
-     * 
-     * Save that deleted character onto a Stack of Characters.
-     * 
-     * Choose a key to be the Undo key. Make it so that when that key is
-     * pressed, the top Character is popped  off the Stack and added back to
-     * the JLabel.
-     */
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
+	/*
+	 * Create a JFrame with a JPanel and a JLabel.
+	 * 
+	 * Every time a key is pressed, add that character to the JLabel. It should look
+	 * like a basic text editor.
+	 * 
+	 * Make it so that every time the BACKSPACE key is pressed, the last character
+	 * is erased from the JLabel.
+	 * 
+	 * Save that deleted character onto a Stack of Characters.
+	 * 
+	 * Choose a key to be the Undo key. Make it so that when that key is pressed,
+	 * the top Character is popped off the Stack and added back to the JLabel.
+	 */
+	Stack<Character> deletedChars = new Stack<Character>();
+	JLabel label;
+	_02_TextUndoRedo() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		label = new JLabel();
+		panel.add(label);
+		frame.add(panel);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.addKeyListener(this);
+		frame.pack();
+	}
+
+	public static void main(String[] args) {
+		new _02_TextUndoRedo();
+	}
+
+	@Override
+	public void keyPressed(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		if (arg0.getKeyChar() == '/') {
+			label.setText(label.getText() + deletedChars.pop());
+		} else if (arg0.getKeyCode() == KeyEvent.VK_BACK_SPACE) {
+			deletedChars.push(label.getText().charAt(label.getText().length() - 1));
+			label.setText(label.getText().substring(0, label.getText().length() - 1));
+		}else {
+			label.setText(label.getText() + arg0.getKeyChar());
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void keyTyped(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
